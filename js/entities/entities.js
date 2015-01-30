6e3705584493018b0ef119e4e59440d5055f16e3
Burma Brown <brownburma@gmail.com>
2015-01-30 12:08:05 -0800

    full Video 12 code

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,7 +13,7 @@ game.PlayerEntity = me.Entity.extend ({
               
             }]);
             this.body.setVelocity(5, 20);// x and y value
-          
+            me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
             this.renderable.addAnimation("idle", [78]);
             this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
             this.renderable.setCurrentAnimation("idle");
@@ -62,8 +62,11 @@ game.PlayerBaseEntity = me.Entity.extend({
                 this.health = 10;
                 this.alwaysUpdate = true;
                 this.body.onCollision = this.onCollision.bind(this);
-                
                 this.type = "PlayerBaseEntity";
+                
+                this.renderable.addAnimation("idle", [0]);
+                this.renderable.addAnimation("broken", [1]);
+                this.renderable.setCurrentAnimation("idle");
                 
                 
         },
@@ -71,6 +74,7 @@ game.PlayerBaseEntity = me.Entity.extend({
         update:function(delta){
             if(this.health<=0){
                 this.broken = true;
+                this.renderable.setCurrentAnimation("broken");
             }
             this.body.update(delta);
             
@@ -99,8 +103,11 @@ game.EnemyBaseEntity = me.Entity.extend({
                 this.health = 10;
                 this.alwaysUpdate = true;
                 this.body.onCollision = this.onCollision.bind(this);
-                
                 this.type = "EnemyBaseEntity";
+                
+                this.renderable.addAnimation("idle", [0]);
+                this.renderable.addAnimation("broken", [1]);
+                this.renderable.setCurrentAnimation("idle");
                 
                 
         },
@@ -108,6 +115,8 @@ game.EnemyBaseEntity = me.Entity.extend({
         update:function(delta){
             if(this.health<=0){
                 this.broken = true;
+                this.renderable.setCurrentAnimation("broken");
+                
             }
             this.body.update(delta);
             
